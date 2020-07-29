d46fadd904d307ebbb47c520f5f8f77acb3ab7d9
Andres Forero <afforeroc@unal.edu.co>
2020-07-28 22:42:54 -0500

    section1to11 - resume

--- hola-mundo/js/01-hola-mundo.js
+++
@@ -1,4 +0,0 @@
-alert("Hola Mundo con JS");
-alert("Bienvenido al curso");
-console.log("Muestro esto en la consola");
-console.log(88+4);
---
+++ section1to11/section1to11.js
@@ -0,0 +1,61 @@
+// Best practices
+'use strict'
+
+
+// Output functions
+//alert("Hola Mundo con JS");
+//console.log(8+1);
+//console.log("Hola\n");
+//document.write("<h1>Tabla del " + c + "</h1>");
+
+
+// Input functions
+//var input = prompt("Que edad tienes?", 18);
+
+
+// Data storage: var, let, const
+var pais = "España"; // Global scope
+let prueba = true; // Local scope
+const pi = 3.1416; // Never change
+
+
+// Data types
+number
+string
+boolean
+typeof var_value
+
+
+// Cast/Check values
+Number()
+parseInt()
+parseFloat()
+isNaN(pais); // Check if not null
+
+
+// Operators
++, -, /, %, >, <, !, ==, !=, === 
+
+
+// 'if/else' - control structure
+if (condition) {
+    ...
+} else if {
+    ...
+} else {
+    ...
+} 
+
+
+//  'for' loop
+for (var i=0; i<value; i++) {
+    ...
+}
+
+
+// 'while' loop
+initialCondition
+while (condition){
+    ...
+    increment
+}
